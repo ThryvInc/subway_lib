9ca3621ecc69ffcabb53db53970a463bd2b26273
Elliot <elliot.schrock@gmail.com>
2014-12-21 17:06:08 -0500

    revert to module due to weird jank from AAR

--- install-mvn-local.sh
+++
@@ -1,6 +0,0 @@
-mvn install:install-file \
--Dfile=library/build/outputs/aar/library-0.1.aar \
--DgroupId=com.rndapp \
--DartifactId=library \
--Dversion=0.1 \
--Dpackaging=aar
--- library/src/main/java/com/rndapp/subway_lib/Line.java
+++
@@ -1,8 +0,0 @@
-package com.rndapp.subway_lib;
-
-/**
- * Created by kevin on 7/16/14.
- */
-public interface Line {
-    public String getName();
-}
